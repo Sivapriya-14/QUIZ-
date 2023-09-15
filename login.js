7e47868b23222bd36d695aa3f499a2b7223749ba
Sivapriya Rajendran <100206921+Sivapriya-14@users.noreply.github.com>
2023-09-15 11:49:16 +0530

    Add files via upload
    Login page with Quiz 
    Technology Used : HTML 5, CSS 3, BOOTSTRAP 5 and JAVASCRIPT

---
+++ login.js
@@ -0,0 +1,6 @@
+function password()
+{
+
+    alert(
+    " Maximum length more than 8 characters\n At least one uppercase letter[A-Z]\n Atleast one lowercase letter[a-z]\n Atleast one special character[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]");
+}
